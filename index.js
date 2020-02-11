351febdc59e88ce72bb97a303f50aa45386828a8
natestock <nathanestock@gmail.com>
2020-02-11 11:06:02 -0500

    add device

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {DSLink, RootNode, ActionNode} = require("dslink");
-//const request = require('request');
+const request = require('request');
 //const rpn = require('request-promise-native');
 
 class Eclypse extends RootNode {
@@ -16,22 +16,28 @@ class AddDevice extends ActionNode {
   }
   onInvoke(params, parentNode) {
     let {IP} = params;
+    let device = this.getDevice(IP);
+    if (device) {
+      let device = this.createChild(device.hostId, Device);
+    } else {
+      throw Error('Invalid device');
+    }
   }
-}
-/*
-class Device extends BaseLocalNode {
-  constructor(path, provider) {
-    super(path, provider);
-    this.authenticate();
-  }
-  authenticate() {
-    request.get('endpoint').auth('username', 'password', true)
+  getDevice(ip) {
+    request.get('endpoint').auth('admin', 'Maxair814', true)
     .on('response', (response) => {
       console.log(response);
     });
+    return null;
   }
 }
-*/
+
+class Device extends BaseLocalNode {
+  constructor(path, provider) {
+    super(path, provider);
+  }
+}
+
 async function main() {
   let rootNode = new Eclypse();
   let link = new DSLink('ECLYPSE', { rootNode });
