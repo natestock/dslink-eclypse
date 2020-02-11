ecbf97e3a653e7fc93e6a16bd23eec0fcee12f72
natestock <nathanestock@gmail.com>
2020-02-11 10:16:52 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-# template-dslink-javascript
-A template to kickstart creating a DSLink using the JavaScript SDK.
+# dslink-eclypse
+DSLink to communicate with Distech Eclypse controllers over REST API.
 
 ## Install nodejs
 please install nodejs version 10 or higher before installing the dslink
 
 https://nodejs.org/en/download/package-manager/
-
-## Creating zip package
-
-creating a zip with these files so you can install it to the dsa broker through the sys action at `/sys/links/Install from Zip`
-
-- dslink.json
-- package.json
-- install.js
-- index.js
-- any other js file you created
-
 
--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const {DSLink, RootNode, BaseLocalNode, ValueNode, Permission} = require("dslink");
+const {DSLink, RootNode, BaseLocalNode, ActionNode} = require("dslink");
+const request = require('request');
+const rpn = require('request-promise-native');
+
+class Eclypse extends RootNode {
+  initialize() {
+    this.createChild('Add Device', AddDevice);
+  }
+}
+
+class AddDevice extends ActionNode {
+  initialize() {
+    this.setConfig('$params', [
+      {name: 'IP', type: 'string'},
+      {name: 'username', type: 'string'},
+      {name: 'password', type: 'string'}
+    ]);
+  }
+  onInvoke(params, parentNode) {
+    let {IP} = params;
+  }
+}
 
 class Device extends BaseLocalNode {
   constructor(path, provider) {
     super(path, provider);
-    
+    this.authenticate();
   }
   authenticate() {
-
+    request.get('endpoint').auth('username', 'password', true)
+    .on('response', (response) => {
+      console.log(response);
+    });
   }
 }
 
 function main() {
-  let rootNode = new RootNode();
-  rootNode.createChild('value', MyValueNode);
-  let link = new DSLink('mydslink', {rootNode});
+  let eclypseRoot = new Eclypse();
+  let link = new DSLink('ECLYPSE', {eclypseRoot});
   link.connect();
 }
 
